b2c4c5eab4eb1383455f2e523f3130f45eaf34b1
supershuaifeng <limchinfeng@gmail.com>
2024-01-04 23:07:35 +0800

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
+  <app-header></app-header>
   `,
   styles: []
 })
